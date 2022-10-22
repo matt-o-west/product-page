7160c2fba6269fc0ee5444e52470377d331dbb01
matt-o-west <matt.omalley.west@gmail.com>
2022-10-21 21:43:29 -0400

    working on product page

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import './ProductPage.css'
 
 const ProductPage = () => {
-  return <div>ProductPage</div>
+  return (
+    <>
+      <div className='page'>
+        <div className='productImage'></div>
+        <div className='productInfo'>
+          <h1>Product Name</h1>
+          <p>Product Description</p>
+          <p>Product Price</p>
+          <button className='addToCart'>Add to Cart</button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default ProductPage
