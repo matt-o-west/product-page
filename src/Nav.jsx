a0b82e76a009fa69d7d71478ab738180bdef29bf
matt-o-west <matt.omalley.west@gmail.com>
2022-10-21 20:23:22 -0400

    style navbar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import './Nav.css'
 
 const Nav = () => {
   return (
     <nav>
-      <img src='logo.svg' alt='company logo' />
-      <button>Collections</button>
-      <button>Men</button>
-      <button>Women</button>
-      <button>About</button>
-      <button>Contact</button>
+      <img src='logo.svg' alt='company logo' id='logo' />
+      <button className='navButton'>Collections</button>
+      <button className='navButton'>Men</button>
+      <button className='navButton'>Women</button>
+      <button className='navButton'>About</button>
+      <button className='navButton'>Contact</button>
       <div className='ui'>
-        <img src='icon-cart.svg' aria-hidden='true' />
-        <img src='image-avatar.png' alt='avatar' />
+        <img src='icon-cart.svg' aria-hidden='true' id='cart' />
+        <img src='image-avatar.png' alt='avatar' id='avatar' />
       </div>
     </nav>
   )
