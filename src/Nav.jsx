9a0ebb49f5500398555bad7e9986b7afc1931c8a
matt-o-west <matt.omalley.west@gmail.com>
2022-10-25 22:16:53 -0400

    add mui core and begin work on cart modal

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
 import './Nav.css'
+import { Box, Modal, Typography } from '@material-ui/core'
+import Button from '@material-ui/core/Button'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleToggleCart = () => {
+    setOpen(!open)
+  }
+
   return (
     <nav>
       <img src='logo.svg' alt='company logo' id='logo' />
@@ -11,7 +19,11 @@ const Nav = () => {
       <button className='navButton'>About</button>
       <button className='navButton'>Contact</button>
       <div className='ui'>
-        <img src='icon-cart.svg' aria-hidden='true' id='cart' />
+        <Button onClick={handleToggleCart}>
+          <img src='icon-cart.svg' aria-hidden='true' id='cart' />
+        </Button>
+
+        <Modal open={open}></Modal>
         <img src='image-avatar.png' alt='avatar' id='avatar' />
       </div>
     </nav>
--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -2,17 +2,53 @@ import React from 'react'
 import './ProductPage.css'
 import Counter from './Counter'
 import AddToCart from './AddToCart'
+import { useState } from 'react'
 
 const ProductPage = () => {
+  const [imageShown, setImageShown] = useState('image-product-1.jpg')
+
+  console.log(imageShown)
+
+  const handleImageClick = (e) => {
+    setImageShown(`image-product-${e.target.id}.jpg`)
+    console.log(e.target.id)
+  }
+
   return (
     <>
       <div className='page'>
-        <div className='productImage'></div>
+        <div className='productImage'>
+          <img className='main-img' src={imageShown} />
+        </div>
         <div className='gallery'>
-          <img className='galleryImage' src='image-product-1-thumbnail.jpg' />
-          <img className='galleryImage' src='image-product-2-thumbnail.jpg' />
-          <img className='galleryImage' src='image-product-3-thumbnail.jpg' />
-          <img className='galleryImage' src='image-product-4-thumbnail.jpg' />
+          <img
+            className='galleryImage'
+            src='image-product-1-thumbnail.jpg'
+            alt='thumbnail image 1'
+            onClick={handleImageClick}
+            id='1'
+          />
+          <img
+            className='galleryImage'
+            src='image-product-2-thumbnail.jpg'
+            alt='thumbnail image 2'
+            onClick={handleImageClick}
+            id='2'
+          />
+          <img
+            className='galleryImage'
+            src='image-product-3-thumbnail.jpg'
+            alt='thumbnail image 3'
+            onClick={handleImageClick}
+            id='3'
+          />
+          <img
+            className='galleryImage'
+            src='image-product-4-thumbnail.jpg'
+            alt='thumbnail image 4'
+            onClick={handleImageClick}
+            id='4'
+          />
         </div>
         <div className='productInfo'>
           <h1>Limited Edition Sneakers</h1>
