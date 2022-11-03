d7e531cc05bf6ca2820692e0908eaf12255daaf2
matt-o-west <matt.omalley.west@gmail.com>
2022-11-02 20:26:50 -0400

    begin configuring firestore

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,44 @@ import ProductPage from './ProductPage'
 import HomePage from './HomePage'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+//Firebase Config
+
+/*
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCp6gvwHYFXVU88hCYjK0CzGPMEqp5f1eA',
+  authDomain: 'shopping-cart-d2e9e.firebaseapp.com',
+  projectId: 'shopping-cart-d2e9e',
+  storageBucket: 'shopping-cart-d2e9e.appspot.com',
+  messagingSenderId: '445024795050',
+  appId: '1:445024795050:web:6a3252c4cea4f4c84f13d3',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+console.log(db)
+*/
 
 function App() {
   const [cart, setCart] = useState([])
   const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    // to visualize the cart in the console every time in changes
+    // you can also use React dev tools
+    console.log(cart)
+  }, [cart])
 
   const handleProductAdd = (newProduct) => {
     console.log('product added to cart')
