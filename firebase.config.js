3b4115a1e7334d4bbef0ea4744b26dfe9ce222c5
matt-o-west <matt.omalley.west@gmail.com>
2022-11-04 12:28:23 -0400

    working on connecting firebase

---
+++ firebase.config.js
@@ -0,0 +1,21 @@
+//Firebase Config
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from '@firebase/app'
+import { getFirestore } from '@firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCp6gvwHYFXVU88hCYjK0CzGPMEqp5f1eA',
+  authDomain: 'shopping-cart-d2e9e.firebaseapp.com',
+  projectId: 'shopping-cart-d2e9e',
+  storageBucket: 'shopping-cart-d2e9e.appspot.com',
+  messagingSenderId: '445024795050',
+  appId: '1:445024795050:web:6a3252c4cea4f4c84f13d3',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
